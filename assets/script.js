f73f9f839575f735159f0d3b57fd573cf2bbce96
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-08-03 23:57:03 -0700

    adding time color logic

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+
+    getCurrentDay();
+
+    pastPresentFuture();
+
+    function pastPresentFuture() {
+        // get current hour
+        const currentHour = moment().hours();
+
+        // target hour description
+        const descriptions = $(".description");
+
+        // current time stored in description block
+        const currentBlock = $(descriptions[0]).attr("data-time");
+
+        // if statement check for time
+        if (currentBlock < currentHour) {
+            $(descriptions[0]).addClass("past");
+        } else if (currentBlock == currentHour) {
+            $(descriptions[0]).addClass("present");
+        }
+
+    }
+
+
+
     function getCurrentDay() {
 
         // current day format
@@ -7,7 +33,7 @@ $(document).ready(function () {
         $("#currentDay").text(currentDay);
     }
 
-    getCurrentDay();
+
 
 })
 
